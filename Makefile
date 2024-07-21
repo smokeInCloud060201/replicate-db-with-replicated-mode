fad4428b6e7aaf7cdb60dc0f99fb5b1a236534aa
Ngọc Khánh <khanhk56a12@gmail.com>
2024-07-21 18:08:24 +0700

    Add k8s

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 
 
+deploys:
+	kubectl apply -f deployment.yaml && kubectl apply -f service.yaml
 
 setup:
 	docker-compose -p replicate up -d
---
+++ src/main/java/vn/com/lol/repilcatedb/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package vn.com.lol.repilcatedb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
